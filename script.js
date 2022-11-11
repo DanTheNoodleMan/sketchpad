80e798648d69b4591c7243fa13d0f11297c65f0f
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-11 21:16:25 +0100

    Completed basic grid creation with deafult size of 16x16

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = "black";
 
-//TODO: function to createGrid
+//TODO:
 //      slider to change grid size (1-64) and call createGrid
 //      color picker (look up how to implement one) --> need option to change color
 
 function createGrid(size = DEFAULT_SIZE){
-    const gridCell = document.createElement("div");
-    gridCell
+    
 
     const gridContainer = document.querySelector(".grid-container"); //select grid container
+    gridContainer.style.display = 'grid';
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`; //make grid container grid with SIZE rows
-    gridContainer.style.gridColumnRows = `repeat(${size}, 1fr)`; //make grid container grid with SIZE columns
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //make grid container grid with SIZE columns
+    
     for(let i = 0; i < size * size; i++){
-        gridContainer.append()
+        const gridCell = document.createElement("div");
+        gridCell.classList.add('cell');
+        gridContainer.appendChild(gridCell);
+        console.log("Div created: " + i);
     }
+    
 }
+
+
+createGrid();
