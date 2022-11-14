7525e243472358e0356c05e6ac7cd978ef459ba5
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-14 11:20:08 +0100

    Basic hover to color the 16x16 grid

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const DEFAULT_COLOR = "black";
 //      slider to change grid size (1-64) and call createGrid
 //      color picker (look up how to implement one) --> need option to change color
 
+const gridContainer = document.querySelector(".grid-container"); //select grid container
+
 function createGrid(size = DEFAULT_SIZE){
     
 
-    const gridContainer = document.querySelector(".grid-container"); //select grid container
+    
     gridContainer.style.display = 'grid';
     gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`; //make grid container grid with SIZE rows
     gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`; //make grid container grid with SIZE columns
@@ -17,10 +19,21 @@ function createGrid(size = DEFAULT_SIZE){
         const gridCell = document.createElement("div");
         gridCell.classList.add('cell');
         gridContainer.appendChild(gridCell);
-        console.log("Div created: " + i);
     }
-    
 }
+
+let cells = document.querySelectorAll(".cell");
+
+
 
 
 createGrid();
+
+cells = document.querySelectorAll(".cell");
+console.log(document.querySelectorAll(".cell"));
+
+cells.forEach( cell => cell.addEventListener('mouseover', function(e){
+    cell.style.backgroundColor = "rgb(255, 0, 0)";
+    console.log(this);
+})
+)
