70fd1f60a03bc69722a81d8aac0c414fdd3a6970
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-14 11:29:35 +0100

    Implemented color picker to draw in different colors

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const DEFAULT_SIZE = 16;
 const DEFAULT_COLOR = "black";
 
+let color = DEFAULT_COLOR;
+
+function updateColor(newColor){
+    color = newColor;
+}
+
 //TODO:
 //      slider to change grid size (1-64) and call createGrid
 //      color picker (look up how to implement one) --> need option to change color
@@ -33,7 +39,12 @@ cells = document.querySelectorAll(".cell");
 console.log(document.querySelectorAll(".cell"));
 
 cells.forEach( cell => cell.addEventListener('mouseover', function(e){
-    cell.style.backgroundColor = "rgb(255, 0, 0)";
+    cell.style.backgroundColor = color;
     console.log(this);
 })
 )
+
+let colorPick = document.getElementById("colorPick");
+colorPick.addEventListener("input", function(){
+  updateColor(colorPick.value);
+}, false);
